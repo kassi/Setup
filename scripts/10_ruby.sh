6302b866b616b8a36a452ed2cde3d44e165f26cb
Karsten Silkenbäumer <karsten.silkenbaeumer@xing.com>
2017-05-16 17:35:47 +0200

    Don't install ruby directly with rvm

--- scripts/10_ruby.sh
+++ scripts/10_ruby.sh
@@ -3,6 +3,5 @@
 # install rvm. ignore dotfiles because it's already been set up by ourselves
 if [[ ! "$(type -P rvm)" ]]; then
   info "Installing RVM"
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
+  \curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles
 fi
-
