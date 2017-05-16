03b25b5ebe552372162c23eb27851ad0cadc3e04
Karsten Silkenbäumer <karsten.silkenbaeumer@xing.com>
2017-05-16 17:49:15 +0200

    Add keyserver check for rvm

--- scripts/10_ruby.sh
+++ scripts/10_ruby.sh
@@ -3,5 +3,6 @@
 # install rvm. ignore dotfiles because it's already been set up by ourselves
 if [[ ! "$(type -P rvm)" ]]; then
   info "Installing RVM"
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable
 fi
