fde62d27b8f5fa45bf5d1b92b49835fd537f9f11
Karsten Silkenbäumer <karsten.silkenbaeumer@xing.com>
2017-05-24 11:46:21 +0200

    Add installation for Aerial screen saver

--- scripts/30_homebrew_alternatives.sh
+++ scripts/30_homebrew_alternatives.sh
@@ -6,3 +6,8 @@ if [[ ! -e /Applications/Karabiner-Elements.app ]]; then
   sleep 5
   open /Volumes/Karabiner-Elements*/*.pkg
 fi
+
+if [[ ! -d "$HOME/Library/Screen Savers/Aerial.saver" ]]; then
+  path=$(curl -sL https://github.com/JohnCoates/Aerial/releases/latest | awk '{ if (match($0, /".*Aerial.zip"/)) print substr($0, RSTART+1, RLENGTH-2) }')
+  (cd $HOME/Downloads && { curl -sLO https://github.com$path; unzip -q Aerial.zip; mv Aerial.saver "$HOME/Library/Screen Savers"; rm Aerial.zip; })
+fi
