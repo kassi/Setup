2087c817b4ecffc9855129c44a4f7da35520a7a6
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2020-05-05 11:15:48 +0200

    Add newly compiled version of stripcmt

--- scripts/26_homebrew_alternatives.sh
+++ scripts/26_homebrew_alternatives.sh
@@ -18,3 +18,15 @@ if [[ ! -d "$HOME/Library/Screen Savers/Aerial.saver" ]]; then
   path=$(curl -sL https://github.com/JohnCoates/Aerial/releases/latest | awk '{ if (match($0, /".*Aerial.zip"/)) print substr($0, RSTART+1, RLENGTH-2) }')
   (cd $HOME/Downloads && { curl -sLO https://github.com$path; unzip -q Aerial.zip; mv Aerial.saver "$HOME/Library/Screen Savers"; rm Aerial.zip; })
 fi
+
+if [[ ! -f "$HOME/bin/stripcmt" ]]; then
+  path=$(curl http://www.bdc.cx/software/stripcmt/ | grep "/download" | cut -d '"' -f 2)
+  wget -P $HOME/Downloads http://www.bdc.cx$path
+  cd $HOME/Downloads
+  file=basename $path
+  dir=basename $path .tar.gz
+  tar xfz $file
+  cd $dir
+  make
+  cp stripcmt "$HOME/bin"
+fi
