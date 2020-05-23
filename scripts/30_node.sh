372ff84c6ff54ec89981d27f1f015c76ed0608ba
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2020-05-23 12:19:39 +0200

    Install yo generators by default

--- scripts/30_node.sh
+++ scripts/30_node.sh
@@ -6,4 +6,6 @@ nvm install 8.9.1
 # install node packages
 brew install yarn --without-node
 
-npm install -g semistandard
+npm install -g \
+  semistandard \
+  yo generator-generator generator-code
