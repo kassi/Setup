c82026eb52d9b9fe8e32ff84797308dc2c11ca37
Karsten Silkenbäumer <993392+kassi@users.noreply.github.com>
2024-07-31 20:56:43 +0200

    fix: remove mackup files on restore when they exist

--- scripts/20_mackup.sh
+++ scripts/20_mackup.sh
@@ -8,6 +8,8 @@ while [[ ! -d "$ICLOUD_PATH/System/Mackup" && ! -d "$ICLOUD_PATH/System/Sync" ]]
 done
 
 read -p "Press ENTER to restore mackup files" < /dev/tty
+[[ -e $HOME/.mackup.cfg ]] && rm $HOME/.mackup.cfg
+[[ -e $HOME/.mackup ]] && rm -rf $HOME/.mackup
 ln -s $DATA_PATH/mackup.cfg $HOME/.mackup.cfg
 ln -s $DATA_PATH/mackup $HOME/.mackup
 mackup restore
