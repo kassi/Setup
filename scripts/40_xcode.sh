9add1d7b95036fd6bbf90575ec0b162d5ea1391c
Karsten Silkenbäumer <993392+kassi@users.noreply.github.com>
2024-08-01 00:15:56 +0200

    fix: run xcode related configs only when it is installed

--- scripts/40_xcode.sh
+++ scripts/40_xcode.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sudo xcodebuild -license accept
+if [[ -d /Applications/Xcode.app ]]; then
+  sudo xcodebuild -license accept
+
+  PlistBuddy=/usr/libexec/PlistBuddy
+
+  IDETextKeyBindingSet=/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist
+  sudo $PlistBuddy -c "Add :Customized dict" $IDETextKeyBindingSet
+  sudo $PlistBuddy -c "Add ':Customized:Delete Line' string" $IDETextKeyBindingSet
+  sudo $PlistBuddy -c "Set ':Customized:Delete Line' 'moveToEndOfLine:, deleteToBeginningOfLine:, deleteToEndOfParagraph:'" $IDETextKeyBindingSet
+  sudo $PlistBuddy -c "Add ':Customized:Duplicate Line' string" $IDETextKeyBindingSet
+  sudo $PlistBuddy -c "Set ':Customized:Duplicate Line' 'selectLine:, deleteToEndOfParagraph:, yank:, moveToLeftEndOfLine:, yank:'" $IDETextKeyBindingSet
+fi
